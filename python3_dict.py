825d52734e9b9c0bd01f658a6262bf7b65b66f07
Dharanesh2612 <119128862+Dharanesh2612@users.noreply.github.com>
2024-07-25 16:32:20 +0530

    Update python3_dict.py

--- python3_dict.py
+++ python3_dict.py
@@ -24,26 +24,6 @@ def main():
         print("5. Exit")
 
         choice = input("\nEnter your choice (1-5): ")
-           """
-              Input Handling:
-
-Prompts the user to enter a choice (1 to 5).
-Checks if the input (choice) is a digit using isdigit().
-Converts choice to an integer (int(choice)).
-Input Validation:
-
-Ensures that the user's input (choice) is between 1 and 5. If not, prompts the user to enter a valid choice.
-Number Input:
-
-Prompts the user to enter two numbers (num1 and num2) for the chosen operation.
-Converts the input numbers to float to handle both integer and floating-point inputs.
-Operation Execution:
-
-Uses a dictionary operations to map the user's choice to the corresponding operation function (add, subtract, multiply, divide).
-Executes the selected operation function with num1 and num2 as arguments.
-Stores the operation name and result in variables operation_name and result respectively from the operations dictionary.
-
-           """
 
 
         if choice == '5':
