3004bce8163d7ddd615a26e5f9b8302853370dfb
Dharanesh2612 <119128862+Dharanesh2612@users.noreply.github.com>
2024-07-26 12:02:02 +0530

    Add files via upload

---
+++ maptask.py
@@ -0,0 +1,27 @@
+#sort a dictionary by value in python (lambda function)by name
+def main():
+    name_list = [
+    {'name':'shreya','age':15},
+    {'name':'pratiksha','age':13},
+    {'name':'prerna','age':15}
+    ]
+
+    name_list.sort(key=lambda x : x['name'])
+    print(name_list)
+if __name__ =="__main__":
+       main()
+
+
+
+#sort a dictionary by value in python (lambda function)by age
+def main():
+    name_list = [
+    {'name':'shreya','age':15},
+    {'name':'pratiksha','age':13},
+    {'name':'prerna','age':15}
+    ]
+
+    name_list.sort(key=lambda x : x['name'])
+    print(name_list)
+if __name__ =="__main__":
+       main()
